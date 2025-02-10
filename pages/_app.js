b2a5367a56aa463605bb130d137cf09ffa0b9447
Melissa <contact@pledgeandgrow.com>
2025-02-10 21:45:03 +0100

    Correction Open Graph pour Facebook

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link rel="icon" href="img/favicon.ico" />
         <meta name="description" content="personal portfolio" />
+        <meta property="og:title" content="Melytop" />
+<meta property="og:description" content="Personal portfolio" />
+<meta property="og:image" content="https://melytop.fr/img/thumb/square.jpg?v=2" />
+<meta property="og:image:alt" content="image" />
+<meta property="og:url" content="https://melytop.fr/" />
+<meta property="og:type" content="website" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
       </Head>
       <Component {...pageProps} />
